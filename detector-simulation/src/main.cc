fccba0fd927ae153d37c8674a027fc2c26ec3d80
samyosm <samy.osmium@gmail.com>
2026-05-08 12:25:53 -0400

    fix attempt: no visuals

--- detector-simulation/src/main.cc
+++ detector-simulation/src/main.cc
@@ -3,39 +3,40 @@
 #include "FTFP_BERT.hh"
 
 #include "G4RunManagerFactory.hh"
+#include "G4UIExecutive.hh"
 #include "G4UImanager.hh"
-#include <G4String.hh>
-#include <G4UIExecutive.hh>
-#include <G4VisExecutive.hh>
-#include <G4VisManager.hh>
+#include "G4VisExecutive.hh"
 
 int main(int argc, char **argv) {
+  G4UIExecutive *ui = nullptr;
+  if (argc == 1) {
+    ui = new G4UIExecutive(argc, argv);
+  }
+
   auto runManager = G4RunManagerFactory::CreateRunManager();
 
   runManager->SetUserInitialization(new DetectorConstruction);
   runManager->SetUserInitialization(new FTFP_BERT);
   runManager->SetUserInitialization(new ActionInitialization);
 
+  G4VisManager *visManager = new G4VisExecutive;
+  visManager->Initialize();
+
   runManager->Initialize();
 
   G4UImanager *UI = G4UImanager::GetUIpointer();
 
-  if (argc != 1) {
+  if (!ui) {
     G4String command = "/control/execute ";
     G4String fileName = argv[1];
-
     UI->ApplyCommand(command + fileName);
   } else {
-    G4VisManager *visManager = new G4VisExecutive;
-    visManager->Initialize();
-
-    G4UIExecutive *ui = new G4UIExecutive(argc, argv);
     UI->ApplyCommand("/control/execute resources/macros/init.mac");
     ui->SessionStart();
-
     delete ui;
   }
 
+  delete visManager;
   delete runManager;
   return 0;
 }
